65fabdb5bcdea05eb71d2484cd6dc1766ca30996
Glenn <glenn.honore@std.heh.be>
2022-01-19 15:20:44 +0100

    file txt

--- loltracker.py
+++ loltracker.py
@@ -5,8 +5,8 @@ import plotly.express as px
 df = px.data.tips()
 fig = px.scatter(df, x=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], y=[1, 2, 3, 5, 1, 6, 8, 4, 2, 11], trendline="ols")
 
-with open('readme.txt') as f:
+with open('LTG_cs_per_min.txt') as f:
     lines = f.readlines()
-    
+
 # showing the plot
 fig.show()

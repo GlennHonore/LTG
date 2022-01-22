5c236647bb9b9002845152dbcff3600e9aa55920
Glenn <glenn.honore@std.heh.be>
2022-01-22 12:44:36 +0100

    add early gold adv and early cs adv

--- loltracker.py
+++ loltracker.py
@@ -21,44 +21,64 @@ def setupData():
 
     df = pd.DataFrame(data={
         'Damage_per_gold':np.array(lines[0]).astype(np.float64), 
-        'CS_per_min':np.array(lines[1]).astype(np.float64)}, 
+        'Early_gold_adv':np.array(lines[1]).astype(np.float64),
+        'Early_cs_adv':np.array(lines[2]).astype(np.float64),
+        'CS_per_min':np.array(lines[3]).astype(np.float64)}, 
     index=xsize)
     return df
 
 def linesValues():
-    filename = ['LTG_damage_per_gold.txt', 'LTG_cs_per_min.txt']
+    filename = ['LTG_damage_per_gold.txt', 'LTG_early_gold_adv.txt', 'LTG_early_cs_adv.txt', 'LTG_cs_per_min.txt']
     lines = []
 
     for i in range(0, len(filename)):
         lines.insert(i, readFile(filename[i]))
     return lines
 
-
 model = LinearRegression()
 lines = linesValues()
 xsize = [j for j in range(0, len(lines[0]))]
 df = setupData()
+fig = make_subplots(rows=2, cols=3)
 
 x_range, y_range = calculateRegression(xsize, 0, lines)
-
-fig = make_subplots(rows=1, cols=2)
 fig.add_trace(
     go.Scatter(x=df.index, y=df.Damage_per_gold, name='Damage per gold'),
-    row=1, col=1
+    row=1, col=2
 )
 fig.add_trace(
-    go.Scatter(x=x_range, y=y_range, name='Regression Fit'),
-    row=1, col=1
+    go.Scatter(x=x_range, y=y_range, name='Regression - Damage per gold'),
+    row=1, col=2
 )
 
 x_range, y_range = calculateRegression(xsize, 1, lines)
+fig.add_trace(
+    go.Scatter(x=df.index, y=df.Early_gold_adv, name='Early gold advantage' ),
+    row=1, col=3
+)
+fig.add_trace(
+    go.Scatter(x=x_range, y=y_range, name='Regression - Early gold adv'), 
+    row=1, col=3
+)
 
+x_range, y_range = calculateRegression(xsize, 2, lines)
+fig.add_trace(
+    go.Scatter(x=df.index, y=df.Early_cs_adv, name='Early CS advantage' ),
+    row=2, col=1
+)
+fig.add_trace(
+    go.Scatter(x=x_range, y=y_range, name='Regression - Early CS adv'), 
+    row=2, col=1
+)
+
+x_range, y_range = calculateRegression(xsize, 3, lines)
 fig.add_trace(
     go.Scatter(x=df.index, y=df.CS_per_min, name='CS per min' ),
-    row=1, col=2
+    row=2, col=2
 )
 fig.add_trace(
-    go.Scatter(x=x_range, y=y_range, name='Regression Fit'), 
-    row=1, col=2
+    go.Scatter(x=x_range, y=y_range, name='Regression - CS per min'), 
+    row=2, col=2
 )
+
 fig.show()
